61c23e850de6b7359c03af791acf113989cb5d77
VoxOel <antchroel@yahoo.com>
2018-04-11 16:36:02 -0400

    added get and set functions

--- src/tetris/HoldBox.java
+++ src/tetris/HoldBox.java
@@ -7,6 +7,29 @@ public class HoldBox {
 	public HoldBox() {
 		hold = '0';
 	}
+        
+        public char getHold()
+        {
+            return hold;
+        }
+        
+        public boolean setHold(char c)
+        {
+            switch(c){
+                case 'i':
+                case 'o':
+                case 't':
+                case 's':
+                case 'z':
+                case 'l':
+                case 'j':
+                    hold = c;
+                    return true;
+                default:
+                    hold = '0';
+                    return false;
+            }
+        }
 	
 	public char swap(char c) {
 		if(hold != '0') {
