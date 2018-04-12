404b43cea0eae29b8fb763bc58bf5ba7168c1a5f
YianniWhisler <yyw16@my.fsu.edu>
2018-04-12 03:47:24 -0500

    updated addScore() according to guidelines

--- src/tetris/Scorecard.java
+++ src/tetris/Scorecard.java
@@ -10,58 +10,89 @@ public class Scorecard {
 
 	/* constructors */
 	Scorecard() {
-		score = 0;
-		level = 1;
-		linesCleared = 0;
-                singles = 0;        //  TODO:
-                doubles = 0;        //      these 5 need
-                triples = 0;        //      getters and
-                tetrises = 0;       //      setters!
-                tSpins = 0;         //
+            score = 0;
+            level = 1;
+            linesCleared = 0;
+            singles = 0;        //  TODO:
+            doubles = 0;        //      these 5 need
+            triples = 0;        //      getters and
+            tetrises = 0;       //      setters!
+            tSpins = 0;         //
 	}
 	
 	Scorecard(int newScore, int newLevel) {
-		score = newScore;
-		level = newLevel;
-		linesCleared = 0;
+            score = newScore;
+            level = newLevel;
+            linesCleared = 0;
 	}
 	
 	/* setters */
 	public void setScore(int newScore) {
-		score = newScore;
+            score = newScore;
 	}
 	
-	public void addScore(int num) {
-		score += num;
+	public void addScore(int linesCleared, boolean TSpin) {
+            /* assumes that this function will probably recieve
+             * parameters from Board or GameManager class.
+             * pseudo:
+             * if(wasTSpin) {Scorecard.addScore(linesCleared, true)}
+             */
+            switch(linesCleared) {
+                case 0: // TSpin
+                    score += 400 * level;
+                    break;
+                case 1: // Single
+                    if(TSpin)
+                        score += 800 * level;
+                    else
+                        score += 100 * level;
+                    break;
+                case 2: // Double
+                    if(TSpin)
+                        score += 1200 * level;
+                    else
+                        score += 300 * level;
+                    break;
+                case 3: // Triple
+                    if(TSpin)
+                        score += 1600 * level;
+                    else
+                        score += 500 * level;
+                    break;
+                case 4: // Tetris!
+                    score += 800 * level;
+                    break;
+            }
+                
 	}
 	
 	public void setLevel(int newLevel) {
-		level = newLevel;
+            level = newLevel;
 	}
 	
 	public void incrementLevel() {
-		level++;
+            level++;
 	}
 	
 	public void setLinesCleared(int newLinesCleared) {
-		linesCleared = newLinesCleared;
+            linesCleared = newLinesCleared;
 	}
 	
 	public void addLinesCleared(int add) {
-		linesCleared += add;
+            linesCleared += add;
 	}
 	
 	/* getters */
 	public int getScore() {
-		return score;
+            return score;
 	}
 	
 	public int getLevel() {
-		return level;
+            return level;
 	}
 	
 	public int getLinesCleared() {
-		return linesCleared;
+            return linesCleared;
 	}
 
 }
