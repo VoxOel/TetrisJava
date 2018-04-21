ed491b3db86aa951c03c5402ce2ac211d93e56e0
VoxOel <antchroel@yahoo.com>
2018-04-21 14:01:53 -0400

    added title screen before game starts

--- src/tetris/Tetris.java
+++ src/tetris/Tetris.java
@@ -1,22 +1,86 @@
 package tetris;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 
-public class Tetris{
+public class Tetris extends JFrame implements KeyListener{
+    
+    private TitleScreen title;
+    private boolean titleShow;
+    
+    private GameManager game;
+    
+    
+    public Tetris()
+    {
+        super("Tetris");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setSize(800, 600);
+        
+        title = new TitleScreen();
+    }
+    
+    public boolean setGM(GameManager gm)
+    {
+        removeKeyListener(game);
+        game = gm;
+        add(game);
+        addKeyListener(game);
+        return true;
+    }
+    
+    public boolean startGame()
+    {
+        game.run();
+        setVisible(true);
+        return true;
+    }
+    
+    public void showTitle()
+    {
+        add(title);
+        setVisible(true);
+        titleShow = true;
+    }
+    
+    public void hideTitle()
+    {
+        remove(title);
+        titleShow = false;
+    }
+    
+    public void initGame()
+    {
+        setGM( new GameManager() );
+        
+        startGame();
+    }
     
     public static void main(String args[]){
-        JFrame frame = new JFrame( "Tetris" );
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Tetris tetris = new Tetris();
         
-        GameManager gm = new GameManager();
-        frame.add( gm );
-        frame.setSize(800, 600);
-        frame.setVisible(true);
+        tetris.showTitle();
+        tetris.addKeyListener(tetris);
         
-        frame.addKeyListener(gm);
-        
-        gm.run();
     }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {}
+
+    @Override
+    public void keyPressed(KeyEvent ke) 
+    {
+        if(titleShow)
+        {
+            hideTitle();
+            initGame();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {}
 
 }
